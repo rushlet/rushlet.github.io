2b08dba7010080b714c88e25cf03b05a044c534f
Becky Rush <becky.rush14@yahoo.co.uk>
2017-10-07 08:46:49 +0100

    create first bar chart

--- source/js/index.js
+++ source/js/index.js
@@ -1,4 +1,20 @@
 import * as d3Request from 'd3-request';
+import * as d3 from "d3";
 
-const message = "HELLO!"
-console.log(`This is written in es6 and working. ${message}`);
+var body = d3.select("body");
+var div = body.append("div");
+div.html("Hello, world!");
+
+var data = [4, 8, 15, 16, 23, 42];
+
+var x = d3.scaleLinear()
+    .domain([0, d3.max(data)])
+    .range([0, 590]);
+
+d3.select(".chart")
+  .selectAll("div")
+    .data(data)
+  .enter().append("div")
+    .style("width", function(d) { return x(d) + "px"; })
+    .classed("bar", true)
+    .text(function(d) { return d; });
