a6e2eb821fcd8d2724c2fae098819f3f07713b27
Becky Rush <becky.rush14@yahoo.co.uk>
2017-10-10 12:40:15 +0100

    pluralsight tutorial

--- source/js/index.js
+++ source/js/index.js
@@ -1,20 +1,21 @@
-import * as d3Request from 'd3-request';
 import * as d3 from "d3";
 
-var body = d3.select("body");
-var div = body.append("div");
-div.html("Hello, world!");
+var w = 300;
+var h = 100;
+var padding = 2;
+var dataset = [5, 10, 14, 20, 25];
 
-var data = [4, 8, 15, 16, 23, 42];
-
-var x = d3.scaleLinear()
-    .domain([0, d3.max(data)])
-    .range([0, 590]);
-
-d3.select(".chart")
-  .selectAll("div")
-    .data(data)
-  .enter().append("div")
-    .style("width", function(d) { return x(d) + "px"; })
-    .classed("bar", true)
-    .text(function(d) { return d; });
+var svg = d3.select("body").append("svg")
+            .attr('width', w)
+            .attr('height', h);
+svg.selectAll('rect')
+  .data(dataset)
+  .enter()
+  .append('rect')
+.attr({
+  x: function(d, index){return index * (w / dataset.length);}),
+  y: function(d){ return h - (d*4); }),
+  width: w / dataset.length - padding,
+  height: funciton(d) { return d*10; },
+  fill: function(d) { return `rgb(${d*10}, 0, 0)`; }
+});
